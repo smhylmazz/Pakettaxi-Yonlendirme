cb70c97834c8ad20aad58d4892c6f94a2da09b30
smhylmazz <ysemih619@gmail.com>
2026-06-27 16:42:49 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,10 @@
-// Giriş sistemi buraya gelecek
+import { auth } from "./firebase-config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log("Giriş yapıldı:", user.email);
+    } else {
+        console.log("Giriş yapılmadı.");
+    }
+});
